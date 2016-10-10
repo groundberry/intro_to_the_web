4921750676a280f7ac39358baaf9e36782b43f76
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-10 18:05:31 +0100

    Return html

--- app.rb
+++ app.rb
@@ -1,8 +1,7 @@
 require 'sinatra'
-require 'shotgun'
 
-get '/secret' do
-  'This is top secret!!!'
+get '/cat' do
+  "<div style='border: 2px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
-
-set :session_secret, 'super secret'
