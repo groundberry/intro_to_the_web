78fe8cc541548c7418edc394f7a937777bd08718
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-11 12:02:52 +0100

    Introduce params

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'sinatra'
 
-get '/cat' do
+get '/random-cat' do
   @random_name = ['Amigo', 'Oscar', 'Viking'].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @random_name = params[:name]
   erb(:index)
 end
